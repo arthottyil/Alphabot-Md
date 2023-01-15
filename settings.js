b33caf1a3d0e8d9259e09456e81f2f0722a16304
arthottyil <122576751+arthottyil@users.noreply.github.com>
2023-01-15 21:55:14 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,24 +31,24 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "ᗪᖇᗩGOᑎ ᵇᵒᵗ" //namabot kalian
+global.ownername = "𝕭𝖑𝖆𝖈𝖐 ʰᵃᵗ 𝕯𝖗𝖆𝖌𝖔𝖓" //nama kalian
+global.myweb = "https://instabio.cc/blackhatdragon" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@techrimo1261" //bebas asal jan hapus
+global.myweb2 = "https://instabio.cc/blackhatdragon" //bebas
+global.email = "blackhatdragon@hotmail.com" //bebas
+global.region = "Kerala" //bebas
+global.timezone = 'Asia/kolkata' //  timezone wib
+global.premium = ['971563736378'] //premium user
+global.owner = ["971563736378",] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "971563736378" // nomor wa kalian
+global.ownernomerr = "+971563736378" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.background_welcome = "https://i.imgur.com/DPbtvrl.jpeg" // maks size 30kb, agar welcome image nya tdk delay
+global.thumbGif = 'https://telegra.ph/file/d95f672d099b84bfd8cfa.mp4'
+global.packname = '© 𝙳𝚁𝙰𝙶𝙾𝙽 𝙱𝙾𝚃' //sticker wm ubah
+global.author = '𝕭𝖑𝖆𝖈𝖐 ʰᵃᵗ 𝕯𝖗𝖆𝖌𝖔𝖓' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
@@ -122,17 +122,17 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-   sukses: '🤗Done, Oke Desu~',
-   admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-   botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-   owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-   group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-   private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-   bot: 'Fitur Khusus Pengguna Nomor Bot',
-   errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-   wait: '⏳ Sedang Di Proses',
-   lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-   example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+   sukses: '🤗Done, Oke Friend~',
+   admin: 'This Command Can Only Be Used By Admin !',
+   botAdmin: 'This command can only be used when the bot becomes admin !',
+   owner: 'This command can only be used by the owner !',
+   group: 'This command can only be used in groups !',
+   private: 'This command can only be used in private !',
+   bot: 'Special Features for Bot Number Users',
+   errtoimg: 'Sorry, Currently Not Supporting Gif Sticker !',
+   wait: '⏳ Being processed',
+   lockCmd: 'Features Not Activated By Owner!',
+   example1: 'Welcome @user in the @subject group Don't forget to read the rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @date (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
 let file = require.resolve(__filename)
